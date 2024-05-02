14d9f3b1a519406d1c7fa6bf0dfdd44d7eddbd24
Akshay <paraba777@gmail.com>
2024-05-03 01:54:57 +0530

    event listener + loading

---
+++ 01-Quote-Generator/Quote-Generator.md
@@ -0,0 +1,27 @@
+# Twitter web
+
+# Web Intent URL
+
+https://twitter.com/intent/tweet
+
+# How TO - CSS Loader
+
+1. <div class="loader"></div>
+
+2. Add CSS -
+
+.loader {
+border: 16px solid #f3f3f3; /_ Light grey _/
+border-top: 16px solid #3498db; /_ Blue _/
+border-radius: 50%;
+width: 120px;
+height: 120px;
+animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+0% { transform: rotate(0deg); }
+100% { transform: rotate(360deg); }
+}
+
+3.
--- 01-Quote-Generator/script.js
+++ 01-Quote-Generator/script.js
@@ -1,15 +1,30 @@
-const quoteContainer = document.querySelector("#quote-generator");
+const quoteContainer = document.querySelector("#quote-container");
 const quoteText = document.querySelector("#quote");
 const authorText = document.querySelector("#author");
 const twitterBtn = document.querySelector("#twitter");
 const newQuoteBtn = document.querySelector("#new-quote");
+const loader = document.querySelector("#loader");
 
-console.log(author);
+// console.log(author);
 
 let apiQuotes = [];
 
+//**Show Loading */
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//**Hide Loading */
+
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // todo : Show new Quotes
 function newQuotes() {
+  loading();
   //todo : Pick random from apiQuotes Array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   quoteText.textContent = quote.text;
@@ -22,15 +37,17 @@ function newQuotes() {
   }
 
   //todo: Check Quote Length for Styling
-  if (quote.text.length > 10) {
+  if (quote.text.length > 120) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
 
   try {
@@ -44,5 +61,16 @@ async function getQuotes() {
   }
 }
 
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}-${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//EventListener
+newQuoteBtn.addEventListener("click", newQuotes);
+twitterBtn.addEventListener("click", tweetQuote);
+
 //**On Load
 getQuotes();
+// loading();
